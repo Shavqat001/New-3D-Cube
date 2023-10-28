3252907af27a85f99d44feb51349df669f054ca1
Shavqat001 <nazriev.shavqatjon@gmail.com>
2023-10-29 00:11:34 +0500

    fix some issues on js

--- app.js
+++ app.js
@@ -1,13 +1,22 @@
 // change color
 
-const getRandomColor = () => {
-    return Math.floor(Math.random() * 999999);
-}
-
 let sides = document.querySelectorAll('.cube__sides');
 
+let colors = [
+    'tomato',
+    'dodgerblue',
+    'gold',
+    'purple',
+    'yellowgreen',
+    'chocolate'
+];
+
+const getRandomNumber = () => {
+    return Math.floor(Math.random() * colors.length);
+}
+
 sides.forEach(el => {
-    el.style.backgroundColor = `#${getRandomColor()}`;
+    el.style.backgroundColor = colors[getRandomNumber()];
 });
 
 // change cube size
@@ -27,7 +36,9 @@ let shapes = [
     'polygon(20% 0%, 0% 20%, 30% 50%, 0% 80%, 20% 100%, 50% 70%, 80% 100%, 100% 80%, 70% 50%, 100% 20%, 80% 0%, 50% 30%)',
     'polygon(50% 0, 60% 40%, 100% 50%, 60% 60%, 50% 100%, 40% 60%, 0 50%, 40% 40%)',
     'polygon(0% 15%, 15% 15%, 15% 0%, 85% 0%, 85% 15%, 100% 15%, 100% 85%, 85% 85%, 85% 100%, 15% 100%, 15% 85%, 0% 85%)',
-    'polygon(0% 0%, 0% 100%, 25% 100%, 25% 25%, 75% 25%, 75% 75%, 25% 75%, 25% 100%, 100% 100%, 100% 0%)'
+    'polygon(0% 0%, 0% 100%, 25% 100%, 25% 25%, 75% 25%, 75% 75%, 25% 75%, 25% 100%, 100% 100%, 100% 0%)',
+    'polygon(20% 0%, 80% 0%, 100% 20%, 100% 80%, 80% 100%, 20% 100%, 0% 80%, 0% 20%)',
+    'polygon(50% 0%, 100% 38%, 82% 100%, 18% 100%, 0% 38%)'
 ];
 
 let shapeButtons = document.querySelectorAll('.shapes__el');
